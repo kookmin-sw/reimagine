6c3128921bcf1c7aa3210e1915df76c913f8d673
RJ <plzzzean@RJui-MacBookPro.local>
2026-03-28 02:06:03 +0900

    Set up GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import History from './pages/History';
 import Survey from './pages/Survey';
